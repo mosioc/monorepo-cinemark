576ac2bed0c2b0b7845a42340ca4996483f00058
Mehdi Maleki <mosioc79@gmail.com>
2025-11-27 00:27:37 +0330

    update AuthForm with better styles and structure
    issue #3

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,20 +1,33 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
+  DefaultValues,
   FieldValues,
+  Path,
   SubmitHandler,
   useForm,
   UseFormReturn,
 } from "react-hook-form";
 import { ZodType } from "zod";
 import { Button } from "@/components/ui/button";
-import { Form } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
 import Link from "next/link";
+import { FIELD_NAMES, FIELD_TYPES } from "@/constants";
 import { useRouter } from "next/navigation";
 
 interface Props<T extends FieldValues> {
   schema: ZodType<T>;
   defaultValues: T;
+  onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
   type: "SIGN_IN" | "SIGN_UP";
 }
 
@@ -22,28 +35,64 @@ const AuthForm = <T extends FieldValues>({
   type,
   schema,
   defaultValues,
+  onSubmit,
 }: Props<T>) => {
   const router = useRouter();
 
   const isSignIn = type === "SIGN_IN";
 
-  const form: UseFormReturn<T> = useForm({});
+  const form: UseFormReturn<T> = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
 
-  const handleSubmit: SubmitHandler<T> = async (data) => {};
+  const handleSubmit: SubmitHandler<T> = async (data) => {
+    const result = await onSubmit(data);
+
+    if (result.success) {
+      router.push("/");
+    } else {
+      alert(result.error ?? "An error occurred.");
+    }
+  };
 
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-semibold text-white">
-        {isSignIn ? "Welcome to CineMark" : "Create your account"}
+        {isSignIn ? "Welcome back to Cinemark" : "Create your cinema account"}
       </h1>
       <p className="text-light-100">
-        {isSignIn ? "Access the movies" : "Please complete all fields"}
+        {isSignIn ? "Access movies!" : "Please complete all fields."}
       </p>
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(handleSubmit)}
           className="w-full space-y-6"
         >
+          {Object.keys(defaultValues).map((field) => (
+            <FormField
+              key={field}
+              control={form.control}
+              name={field as Path<T>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize">
+                    {FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      required
+                      type={FIELD_TYPES[field.name as keyof typeof FIELD_TYPES]}
+                      {...field}
+                      className="form-input"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+
           <Button type="submit" className="form-btn">
             {isSignIn ? "Sign In" : "Sign Up"}
           </Button>
@@ -51,7 +100,7 @@ const AuthForm = <T extends FieldValues>({
       </Form>
 
       <p className="text-center text-base font-medium">
-        {isSignIn ? "New to CineMark? " : "Already have an account? "}
+        {isSignIn ? "New to Cinemark? " : "Already have an account? "}
 
         <Link
           href={isSignIn ? "/sign-up" : "/sign-in"}
