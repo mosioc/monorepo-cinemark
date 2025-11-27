0e00410a51ce6c43cc329fb5b9c2301e37b281bf
Mehdi Maleki <mosioc79@gmail.com>
2025-11-27 16:21:36 +0330

    chore: add redis db and ratelimiter package
    issue #5

--- config.ts
+++ config.ts
@@ -3,6 +3,10 @@ const config = {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
     // prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     databaseUrl: process.env.DATABASE_URL!,
+    upstash: {
+      redisUrl: process.env.UPSTASH_REDIS_REST_URL!,
+      redisToken: process.env.UPSTASH_REDIS_REST_TOKEN!,
+    }
   },
 };
 
