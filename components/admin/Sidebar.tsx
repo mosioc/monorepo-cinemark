1eb121ce79e4c890c4e0b88a2b0b1ee0535cfec6
Mehdi Maleki <mosioc79@gmail.com>
2025-11-27 21:41:56 +0330

    feat: dev admin sidebar
    issue #6

---
+++ components/admin/Sidebar.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import Image from "next/image";
+import { adminSideBarLinks } from "@/constants";
+import Link from "next/link";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { usePathname } from "next/navigation";
+// import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Session } from "next-auth";
+
+const Sidebar = ({ session }: { session: Session }) => {
+  const pathname = usePathname();
+
+  function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+  }
+
+  const getInitials = (name: string): string =>
+    name
+      .split(" ")
+      .map((part) => part[0])
+      .join("")
+      .toUpperCase()
+      .slice(0, 2);
+
+  return (
+    <div className="admin-sidebar">
+      <div>
+        <div className="logo">
+          <Image
+            src="/icons/admin/logo.svg"
+            alt="logo"
+            height={37}
+            width={37}
+          />
+          <h1>Cinamark</h1>
+        </div>
+
+        <div className="mt-10 flex flex-col gap-5">
+          {adminSideBarLinks.map((link) => {
+            const isSelected =
+              (link.route !== "/admin" &&
+                pathname.includes(link.route) &&
+                link.route.length > 1) ||
+              pathname === link.route;
+
+            return (
+              <Link href={link.route} key={link.route}>
+                <div
+                  className={cn(
+                    "link",
+                    isSelected && "bg-primary-admin shadow-sm"
+                  )}
+                >
+                  <div className="relative size-5">
+                    <Image
+                      src={link.img}
+                      alt="icon"
+                      fill
+                      className={`${
+                        isSelected ? "brightness-0 invert" : ""
+                      }  object-contain`}
+                    />
+                  </div>
+
+                  <p className={cn(isSelected ? "text-white" : "text-dark")}>
+                    {link.text}
+                  </p>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="user">
+        {/* <Avatar>
+          <AvatarFallback className="bg-amber-100">
+            {getInitials(session?.user?.name || "IN")}
+          </AvatarFallback>
+        </Avatar> */}
+
+        <div className="flex flex-col max-md:hidden">
+          <p className="font-semibold text-dark-200">{session?.user?.name}</p>
+          <p className="text-xs text-light-500">{session?.user?.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
